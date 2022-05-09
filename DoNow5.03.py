bc63f413783d7db1a1e3484c27df31bbc92ee64f
Vanessa <vdbui25@students.bmchs.org>
2022-05-09 09:35:31 -0500

    Do now 5.03

---
+++ DoNow5.03.py
@@ -0,0 +1,39 @@
+'''
+#################
+Do Now 5.03
+#################
+1. In your Console
+-------------------
+Type the following:
+weekend_dates = {
+  'April 2018': [7, 8, 14, 15, 21, 22, 28, 29],
+  'May 2018': [5, 6, 12, 13, 19, 20, 26, 27]
+}
+​
+print(weekend_dates['April 2018'])
+
+In your Notebook
+----------------
+Respond to the following:
+What type is weekend_dates?
+  A dictionary
+What type the are keys?
+  the month and year
+What type the are values?
+  the days in the list 
+2. In your Console
+-------------------
+Challenge yourself with the following:
+The 2018 Memorial Day holiday is observed on Monday, May 28, which makes it a long-weekend day.
+
+Write a new line of code that adds May 28th to the list associated with 'May 2018'
+
+So that its value becomes [5, 6, 12, 13, 19, 20, 26, 27, 28].
+'''
+
+weekend_dates = {
+  'April 2018': [7, 8, 14, 15, 21, 22, 28, 29],
+  'May 2018': [5, 6, 12, 13, 19, 20, 26, 27, 28]
+}
+
+print(weekend_dates['May 2018'])
